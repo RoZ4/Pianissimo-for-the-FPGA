ba67575e857257143624ff1b67087f3c2248feaa
RoZ <robzupancic@gmail.com>
2024-12-21 23:11:21 -0500

    removed v files

--- FSMs/StartscreenHandler.v
+++
@@ -1,9 +0,0 @@
-`include "../DefineMacros.vh"
-
-module startScreenHandler(clk, inputGetAtAddress, outputColour);
-	input clk; //! Testing clk
-	input [14:0] inputGetAtAddress;
-	output [23:0] outputColour;
-
-	StartScreen startScreenMemoryController (inputGetAtAddress, clk, outputColour);
-endmodule
